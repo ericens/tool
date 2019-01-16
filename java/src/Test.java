9ddf138866b76316c7375934918b4975e835a786
ericens <zenglx@jpush.cn>
2019-01-16 17:44:20 +0800

    update gitbook

--- java/src/Test.java
+++ java/src/Test.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.Date;
+
 public class Test {
     public static void main(String[] args) {
         ThreadLocal threadLocal=new ThreadLocal();
@@ -5,5 +8,13 @@ public class Test {
 
 
 
+        Date d = new Date();
+        // 在默认时区下输出日期和时间值
+        System.out.println(d);
+
+
+        System.out.println(Calendar.getInstance().getTime());
     }
+
 }
+
