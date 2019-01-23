84f3876e85429bfe445f69aceb1e8658afaec42c
ericens <zenglx@jpush.cn>
2019-01-23 15:11:46 +0800

    add

--- java/src/Test.java
+++ java/src/Test.java
@@ -1,12 +1,14 @@
 import java.util.Calendar;
 import java.util.Date;
+import java.util.WeakHashMap;
 
 public class Test {
     public static void main(String[] args) {
         ThreadLocal threadLocal=new ThreadLocal();
         Thread thread=new Thread();
 
-
+        WeakHashMap weakHashMap=new WeakHashMap();
+        weakHashMap.put("sd",222);
 
         Date d = new Date();
         // 在默认时区下输出日期和时间值
